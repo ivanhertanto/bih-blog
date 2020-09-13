c42fa08a008f728a253a27bdc9a61296f51e2a60
Ivan <ivan@hertanto.com>
2020-09-14 01:28:35 +0700

    selesai tahapan basic

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+const Error = () => {
+  return (
+    <Layout>
+      <div>
+        <h3>Oops! Page not found</h3>
+        <h2>sorry, but the page you requested was not found</h2>
+      </div>
+    </Layout>
+  )
+}
+
+export default Error
